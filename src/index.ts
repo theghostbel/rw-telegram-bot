bff0a886ff34ab1b31feee8834fafe0a3a9c80db
Victor Rusakovich <nemiga@gmail.com>
2022-07-08 16:47:47 +0300

    Add alive api

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,11 @@ app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
-  res.json({ Hello: "World" });
+  res.json({ Hello: "World!" });
+});
+
+app.get("/alive", async (req, res) => {
+  res.json({ alive: true });
 });
 
 app.listen(port, () => {
